11a3e40f8e486d41f9adc600ac5c729b2bcb923a
Meenutomar <meenutomar.jt@gmail.com>
2025-05-24 16:13:45 +0530

    Added cache for payment by id

--- service/paymentService.py
+++ service/paymentService.py
@@ -7,12 +7,35 @@ import datetime
 class PaymentService:
     def __init__(self):
         self.payment_cache_data = []
+        
 
     def fetchPaymentById(self, session: Session, id):
+
         startTime = datetime.datetime.now()
         print("-------------------------------Enter DB call", startTime )
-        payment = getPaymentById(session,id)
+        
+        if(len(self.payment_cache_data) == 0):
+            print("Load Cache.....")
+            self.payment_cache_data = get_all_payments(session)
+        
+        print("######################### 1 ############::", len(self.payment_cache_data))
+
+        
+        payment = None
+        for i in range(len(self.payment_cache_data)):
+            if(self.payment_cache_data[i].payment_id == id):
+                print("######################### 2 ############")
+                payment = self.payment_cache_data[i] 
+            
+        print("######################### 3 ############")    
+        if (payment == None):
+            payment = getPaymentById(session,id)
+            self.payment_cache_data.append(payment)
+        
         print("-------------------------------Exit DB call", (datetime.datetime.now()-startTime).total_seconds())
+
+                
+
         payment_read = PaymentDetailView(
             payment_id = payment.payment_id,
             amount = payment.amount,
